92213a9e31055ed9f1c7d4adc6e1ebe2d20becb1
İrem Çetinkol <143891955+Irem-Cetinkol@users.noreply.github.com>
2025-05-28 18:38:44 +0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,5 +1,6 @@
 
 CREATE DATABASE HayvanatBahcesi;
+USE HayvanatBahcesi;
 
 CREATE TABLE Hayvanlar (
     HayvanID INT PRIMARY KEY IDENTITY(1,1),
@@ -29,44 +30,44 @@ CREATE TABLE Kafesler (
 
 INSERT INTO Hayvanlar (Ad, Tur, Cins, DogumTarihi, Cinsiyet, SaglikDurumu, KafesNo)
 VALUES 
-('Aslan Kral', 'Aslan', 'Afrika Aslaný', '2015-05-20', 'Erkek', 'Saðlýklý', 'KAF001'),
-('Maya', 'Fil', 'Asya Fili', '2010-07-12', 'Diþi', 'Diþ problemleri var', 'KAF002'),
-('Çita', 'Çita', 'Afrika Çitasý', '2018-04-22', 'Diþi', 'Saðlýklý', 'KAF003'),
-('Bambam', 'Ayý', 'Kutup Ayýsý', '2013-09-18', 'Erkek', 'Sol bacaðý sakat', 'KAF004'),
-('Nemo', 'Penguen', 'Ýmparator Penguen', '2019-01-10', 'Erkek', 'Saðlýklý', 'KAF005'),
-('Koko', 'Maymun', 'Þempanze', '2016-03-15', 'Diþi', 'Saðlýklý', 'KAF006'),
-('Zazu', 'Kuþ', 'Tukan', '2020-08-05', 'Erkek', 'Tüy dökmesi var', 'KAF007'),
-('Sirius', 'Kaplan', 'Bengal Kaplaný', '2014-11-01', 'Erkek', 'Göz enfeksiyonu', 'KAF008'),
-('Luna', 'Zebra', 'Düz Zebra', '2017-06-09', 'Diþi', 'Saðlýklý', 'KAF009'),
-('Dumbo', 'Fil', 'Afrika Fili', '2011-02-28', 'Erkek', 'Týrnak bakýmý gerekli', 'KAF010');
+('Aslan Kral', 'Aslan', 'Afrika AslanÄ±', '2015-05-20', 'Erkek', 'SaÄŸlÄ±klÄ±', 'KAF001'),
+('Maya', 'Fil', 'Asya Fili', '2010-07-12', 'DiÅŸi', 'DiÅŸ problemleri var', 'KAF002'),
+('Ã‡ita', 'Ã‡ita', 'Afrika Ã‡itasÄ±', '2018-04-22', 'DiÅŸi', 'SaÄŸlÄ±klÄ±', 'KAF003'),
+('Bambam', 'AyÄ±', 'Kutup AyÄ±sÄ±', '2013-09-18', 'Erkek', 'Sol bacaÄŸÄ± sakat', 'KAF004'),
+('Nemo', 'Penguen', 'Ä°mparator Penguen', '2019-01-10', 'Erkek', 'SaÄŸlÄ±klÄ±', 'KAF005'),
+('Koko', 'Maymun', 'Åžempanze', '2016-03-15', 'DiÅŸi', 'SaÄŸlÄ±klÄ±', 'KAF006'),
+('Zazu', 'KuÅŸ', 'Tukan', '2020-08-05', 'Erkek', 'TÃ¼y dÃ¶kmesi var', 'KAF007'),
+('Sirius', 'Kaplan', 'Bengal KaplanÄ±', '2014-11-01', 'Erkek', 'GÃ¶z enfeksiyonu', 'KAF008'),
+('Luna', 'Zebra', 'DÃ¼z Zebra', '2017-06-09', 'DiÅŸi', 'SaÄŸlÄ±klÄ±', 'KAF009'),
+('Dumbo', 'Fil', 'Afrika Fili', '2011-02-28', 'Erkek', 'TÄ±rnak bakÄ±mÄ± gerekli', 'KAF010');
 
 
 
 INSERT INTO Ziyaretciler (AdSoyad, GirisTarihi, CikisTarihi, BiletTipi)
 VALUES
-('Ahmet Yýlmaz', '2025-05-28 10:00:00', '2025-05-28 13:00:00', 'Tam'),
-('Elif Demir', '2025-05-28 11:15:00', '2025-05-28 12:45:00', 'Öðrenci'),
-('Zeynep Kara', '2025-05-28 09:45:00', NULL, 'Çocuk'),
-('Mert Þahin', '2025-05-28 14:00:00', '2025-05-28 16:30:00', 'Tam'),
-('Ayþe Aydýn', '2025-05-28 13:30:00', '2025-05-28 15:00:00', 'Tam'),
-('Kerem Aksoy', '2025-05-28 15:00:00', NULL, 'Öðrenci'),
-('Deniz Kýlýç', '2025-05-28 10:30:00', '2025-05-28 12:30:00', 'Tam'),
-('Merve Güneþ', '2025-05-28 11:00:00', '2025-05-28 13:00:00', 'Öðrenci'),
-('Ali Vural', '2025-05-28 09:00:00', '2025-05-28 11:00:00', 'Çocuk'),
+('Ahmet YÄ±lmaz', '2025-05-28 10:00:00', '2025-05-28 13:00:00', 'Tam'),
+('Elif Demir', '2025-05-28 11:15:00', '2025-05-28 12:45:00', 'Ã–ÄŸrenci'),
+('Zeynep Kara', '2025-05-28 09:45:00', NULL, 'Ã‡ocuk'),
+('Mert Åžahin', '2025-05-28 14:00:00', '2025-05-28 16:30:00', 'Tam'),
+('AyÅŸe AydÄ±n', '2025-05-28 13:30:00', '2025-05-28 15:00:00', 'Tam'),
+('Kerem Aksoy', '2025-05-28 15:00:00', NULL, 'Ã–ÄŸrenci'),
+('Deniz KÄ±lÄ±Ã§', '2025-05-28 10:30:00', '2025-05-28 12:30:00', 'Tam'),
+('Merve GÃ¼neÅŸ', '2025-05-28 11:00:00', '2025-05-28 13:00:00', 'Ã–ÄŸrenci'),
+('Ali Vural', '2025-05-28 09:00:00', '2025-05-28 11:00:00', 'Ã‡ocuk'),
 ('Buse Tan', '2025-05-28 14:15:00', '2025-05-28 16:00:00', 'Tam');
 
 
 
 INSERT INTO Kafesler (KafesNo, Alan, HayvanSayisi, TemizlikDurumu)
 VALUES
-('KAF001', 'Orman Bölgesi', 1, 'Temiz'),
+('KAF001', 'Orman BÃ¶lgesi', 1, 'Temiz'),
 ('KAF002', 'Tropik Alan', 1, 'Temizlik Gerekli'),
-('KAF003', 'Açýk Arazi', 1, 'Temiz'),
-('KAF004', 'Kutupsal Alan', 1, 'Bakýmda'),
+('KAF003', 'AÃ§Ä±k Arazi', 1, 'Temiz'),
+('KAF004', 'Kutupsal Alan', 1, 'BakÄ±mda'),
 ('KAF005', 'Buzul Alan', 1, 'Temiz'),
-('KAF006', 'Yaðmur Ormaný', 1, 'Kirli'),
-('KAF007', 'Kuþ Kafesi', 1, 'Temiz'),
-('KAF008', 'Kaplan Alaný', 1, 'Temizlik Gerekli'),
-('KAF009', 'Savan Bölgesi', 1, 'Temiz'),
-('KAF010', 'Fil Alaný', 1, 'Temiz');
+('KAF006', 'YaÄŸmur OrmanÄ±', 1, 'Kirli'),
+('KAF007', 'KuÅŸ Kafesi', 1, 'Temiz'),
+('KAF008', 'Kaplan AlanÄ±', 1, 'Temizlik Gerekli'),
+('KAF009', 'Savan BÃ¶lgesi', 1, 'Temiz'),
+('KAF010', 'Fil AlanÄ±', 1, 'Temiz');
 
